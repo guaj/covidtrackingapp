a270dcbc18dbc731d2e245b78d884aac3991a710
Jasmin <60197474+guaj@users.noreply.github.com>
2022-02-14 22:02:10 -0500

    Revert "Doctor patient list"

--- src/components/Dashboard/RunDashPro.js
+++ src/components/Dashboard/RunDashPro.js
@@ -1,17 +1,14 @@
 import NavbarDashboardPro from '../Navbar/NavbarDashboardPro';
 import Sidebar from './Sidebar';
 import DashboardPro from './DashboardPro';
-import "./RunDashPro.css"
 
 //  Used to show the sidebar and Dashboard with the FaceInfo and Chart
 export default function runDashPro() {
   return (
     <div>
-
       <NavbarDashboardPro/>
-        <h1 className="pageHeader">Dashboard</h1>
     <div className="container">
-      {/*  <Sidebar/>   To be removed*/}
+        <Sidebar/>
         <DashboardPro/>
     </div>
     </div>
--- src/components/SignUpPatient.js
+++ src/components/SignUpPatient.js
@@ -8,7 +8,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import geometricImage from "../../../images/geometric_gradient.jpg";
+import geometricImage from "../images/geometric_gradient.jpg";
   
   const useStyles = makeStyles((theme) => ({
     image: {
