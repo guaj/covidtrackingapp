29d4b85c3bb953f17d18ee64ec13f5a6d45e0992
Jasmin <60197474+guaj@users.noreply.github.com>
2022-02-21 22:00:16 -0500

    Update Dashboard.js
    remove unused imports

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import NavbarDashboardPro from '../Navbar/NavbarDashboardPro';
 //tab components 
 import DoctorTabs from './DoctorDashboard/DoctorTabs';
 import PatientTabs from './PatientDashboard/PatientTabs';
