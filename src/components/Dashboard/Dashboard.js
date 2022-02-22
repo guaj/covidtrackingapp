0a2076627becc365bccaf264a18b5e9bb221b267
Lucas Catchlove <lucascatchlove@gmail.com>
2022-02-21 19:29:45 -0500

    put dashboard files into appropriate folders

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import NavbarDashboardPro from '../Navbar/NavbarDashboardPro';
 //tab components 
-import DoctorTabs from './DoctorTabs';
-import PatientTabs from './PatientTabs';
-import HealthOfficialTabs from './HealthOfficialTabs';
-import ImmigrationOfficialTabs from './ImmigrationOfficialTabs'
+import DoctorTabs from './DoctorDashboard/DoctorTabs';
+import PatientTabs from './PatientDashboard/PatientTabs';
+import HealthOfficialTabs from './HealthOfficialDashboard/HealthOfficialTabs';
+import ImmigrationOfficialTabs from './ImmigrantOfficerDashboard/ImmigrationOfficialTabs'
 
 const mockProfiles = [
     { name: "Tony Soprano", userType: "doctor" },
