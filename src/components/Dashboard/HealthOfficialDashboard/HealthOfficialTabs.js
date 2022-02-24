9ed08881cf5b57dac8e4d14d0e12e7d91d949e5b
mnash <m.nashatelsayed@gmail.com>
2022-02-24 00:29:22 -0500

    - added Dashboard component tests

--- src/components/Dashboard/HealthOfficialDashboard/HealthOfficialTabs.js
+++ src/components/Dashboard/HealthOfficialDashboard/HealthOfficialTabs.js
@@ -64,7 +64,7 @@ export default function PatientTabs() {
     return (
         <>
             <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                <Tab label="s=Statistics" {...a11yProps(0)} />
+                <Tab label="Statistics" {...a11yProps(0)} />
                 <Tab label="Patient List" {...a11yProps(1)} />
                 <Tab label="Doctor-Patient Pairing List" {...a11yProps(2)} />
             </Tabs>
---
+++ src/components/__tests__/Dashboard.test.js
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import DoctorTabs from '../Dashboard/DoctorDashboard/DoctorTabs';
+import PatientTabs from '../Dashboard/PatientDashboard/PatientTabs';
+import HealthOfficialTabs from '../Dashboard/HealthOfficialDashboard/HealthOfficialTabs'
+import ImmigrationOfficialTabs from '../Dashboard/ImmigrantOfficerDashboard/ImmigrationOfficialTabs'
+
+test("if the patient list tab is rendered when the user is a doctor", () => {
+    render(<DoctorTabs />);
+    expect(screen.getByText("Patient List")).toBeInTheDocument();
+});
+
+test("if the statistics tab is rendered when the user is a doctor", () => {
+    render(<DoctorTabs />);
+    expect(screen.getByText("Statistics")).toBeInTheDocument();
+});
+
+test("if the patient Emergency list tab is rendered when the user is a doctor", () => {
+    render(<DoctorTabs />);
+    expect(screen.getByText("Patient Emergency List")).toBeInTheDocument();
+});
+
+test("if the Doctor-Patient Pairing List is rendered when the user is a doctor", () => {
+    render(<DoctorTabs />);
+    expect(screen.getByText("Doctor-Patient Pairing List")).toBeInTheDocument();
+});
+
+test("if the covid information tab is rendered when the user is a patient", () => {
+    render(<PatientTabs />);
+    expect(screen.getByText("Covid Information")).toBeInTheDocument();
+});
+
+test("if the patient list tab is rendered when the user is a health official", () => {
+    render(<HealthOfficialTabs />);
+    expect(screen.getByText("Patient List")).toBeInTheDocument();
+});
+
+test("if the statistics tab is rendered when the user is a health official", () => {
+    render(<HealthOfficialTabs />);
+    expect(screen.getByText("Statistics")).toBeInTheDocument();
+});
+
+test("if the Doctor-Patient Pairing List is rendered when the user is a health official", () => {
+    render(<HealthOfficialTabs />);
+    expect(screen.getByText("Doctor-Patient Pairing List")).toBeInTheDocument();
+});
+
+test("if the patient list tab is rendered when the user is am immigration official", () => {
+    render(<ImmigrationOfficialTabs />);
+    expect(screen.getByText("Patient List")).toBeInTheDocument();
+});
+
+test("if the statistics tab is rendered when the user is an immigration official", () => {
+    render(<ImmigrationOfficialTabs />);
+    expect(screen.getByText("Statistics")).toBeInTheDocument();
+});
+
+test("if the Doctor-Patient Pairing List is rendered when the user is an immigration official", () => {
+    render(<ImmigrationOfficialTabs />);
+    expect(screen.getByText("Doctor-Patient Pairing List")).toBeInTheDocument();
+});
