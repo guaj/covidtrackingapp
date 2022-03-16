9dd2c2c18cf09666636a0f59ce286879245f83fe
Jasmin <jasmin.guay@mail.concordia.ca>
2022-03-16 16:01:10 -0400

    Resolving build issues

--- src/components/Dashboard/DoctorDashboard/DoctorScheduleDynamoDBAdapter.js
+++ src/components/Dashboard/DoctorDashboard/DoctorScheduleDynamoDBAdapter.js
@@ -2,7 +2,11 @@ import React from "react";
 import AWS from 'aws-sdk';
 import awsConfig from '../../../aws-config.json';
 
-AWS.config.update(awsConfig);
+try {
+    AWS.config.update(awsConfig);
+}catch (e) {}
+
+
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
