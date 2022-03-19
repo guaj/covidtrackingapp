2836d68af2a3a465a86e921146b28404107ef5d6
mnash <m.nashatelsayed@gmail.com>
2022-03-18 22:49:56 -0400

    - added fix to handle some string fields in the database when the field is non-existent for a specific patient; needed in the case where the database is updated on user information submission - added partial implementation for database update when user information is submitted

--- src/Services/ProfileUpdateSercices/PatientProfileUpdate/PatientProfileUpdate.js
+++ src/Services/ProfileUpdateSercices/PatientProfileUpdate/PatientProfileUpdate.js
@@ -70,19 +70,19 @@ export default function ProfilePatient() {
         } finally {
             const formValues = {
                 id: 1,
-                firstName: result.Items.at(0).firstName,
-                lastName: result.Items.at(0).lastName,
-                dob: result.Items.at(0).dob,
+                firstName: (result.Items.at(0).firstName !== undefined ? result.Items.at(0).firstName : ""),
+                lastName: (result.Items.at(0).lastName !== undefined ? result.Items.at(0).lastName : ""),
+                dob: (result.Items.at(0).dob !== undefined ? result.Items.at(0).dob : ""),
                 streetNumber: result.Items.at(0).address.streetNumber,
                 streetName: result.Items.at(0).address.streetName,
                 apartmentNumber: result.Items.at(0).address.apartmentNumber,
                 postalCode: result.Items.at(0).address.postalCode,
                 city: result.Items.at(0).address.city,
                 province: result.Items.at(0).address.province,
-                phoneNumber: result.Items.at(0).phoneNumber,
+                phoneNumber: (result.Items.at(0).phoneNumber !== undefined ? result.Items.at(0).phoneNumber : ""),
                 email: result.Items.at(0).email,
-                ramQNumber: result.Items.at(0).ramQNumber,
-                insurance: result.Items.at(0).insurance,
+                ramQNumber: (result.Items.at(0).ramQNumber !== undefined ? result.Items.at(0).ramQNumber : ""),
+                insurance: (result.Items.at(0).insurance !== undefined ? result.Items.at(0).insurance : ""),
                 symptom1: (result.Items.at(0).symptoms !== undefined ? (result.Items.at(0).symptoms.symptom1 !== undefined ? result.Items.at(0).symptoms.symptom1 : false) : false),
                 symptom2: (result.Items.at(0).symptoms !== undefined ? (result.Items.at(0).symptoms.symptom1 !== undefined ? result.Items.at(0).symptoms.symptom2 : false) : false),
                 symptom3: (result.Items.at(0).symptoms !== undefined ? (result.Items.at(0).symptoms.symptom1 !== undefined ? result.Items.at(0).symptoms.symptom3 : false) : false),
@@ -94,13 +94,84 @@ export default function ProfilePatient() {
                 symptom9: (result.Items.at(0).symptoms !== undefined ? (result.Items.at(0).symptoms.symptom1 !== undefined ? result.Items.at(0).symptoms.symptom9 : false) : false),
                 symptom10: (result.Items.at(0).symptoms !== undefined ? (result.Items.at(0).symptoms.symptom1 !== undefined ? result.Items.at(0).symptoms.symptom10 : false) : false),
                 symptom11: (result.Items.at(0).symptoms !== undefined ? (result.Items.at(0).symptoms.symptom1 !== undefined ? result.Items.at(0).symptoms.symptom11 : false) : false),
-                comments: result.Items.at(0).comments,
+                comments: (result.Items.at(0).comments !== undefined ? result.Items.at(0).comments : ""),
                 doctorId: result.Items.at(0).doctorId,
-                flag: result.Items.at(0).flag
+                flag: (result.Items.at(0).flag !== undefined ? result.Items.at(0).flag : false)
             };
             return [formValues];
         }
     }
+
+    async function updateData(tableName, data) {
+        const params = {
+            TableName: tableName,
+            Key: {"email": data.email},
+            UpdateExpression: "set firstName = :firstName," +
+                "lastName = :lastName," +
+                "dob = :dob," +
+                // "address.streetNumber = :streetNumber," +
+                // "address.streetName = :streetName," +
+                // "address.apartmentNumber = :apartmentNumber," +
+                // "address.postalCode = :postalCode," +
+                // "address.city = :city," +
+                // "address.province = :province," +
+                "phoneNumber = :phoneNumber," +
+                "ramQNumber = :ramQNumber," +
+                "insurance = :insurance," +
+                // "symptoms.symptom1 = :symptom1," +
+                // "symptoms.symptom2 = :symptom2," +
+                // "symptoms.symptom3 = :symptom3," +
+                // "symptoms.symptom4 = :symptom4," +
+                // "symptoms.symptom5 = :symptom5," +
+                // "symptoms.symptom6 = :symptom6," +
+                // "symptoms.symptom7 = :symptom7," +
+                // "symptoms.symptom8 = :symptom8," +
+                // "symptoms.symptom9 = :symptom9," +
+                // "symptoms.symptom10 = :symptom10," +
+                // "symptoms.symptom11 = :symptom11," +
+                "comments = :comments," +
+                "flag = :flag",
+            ExpressionAttributeValues: {
+                ":firstName": data.firstName,
+                ":lastName": data.lastName,
+                ":dob": data.dob,
+                // ":streetNumber": data.streetNumber,
+                // ":streetName": data.streetName,
+                // ":apartmentNumber": data.apartmentNumber,
+                // ":postalCode": data.postalCode,
+                // ":city": data.city,
+                // ":province": data.province,
+                ":phoneNumber": data.phoneNumber,
+                ":ramQNumber": data.ramQNumber,
+                ":insurance": data.insurance,
+                // ":symptom1": data.symptom1,
+                // ":symptom2": data.symptom2,
+                // ":symptom3": data.symptom3,
+                // ":symptom4": data.symptom4,
+                // ":symptom5": data.symptom5,
+                // ":symptom6": data.symptom6,
+                // ":symptom7": data.symptom7,
+                // ":symptom8": data.symptom8,
+                // ":symptom9": data.symptom9,
+                // ":symptom10": data.symptom10,
+                // ":symptom11": data.symptom11,
+                ":comments": data.comments,
+                ":flag": data.flag
+            },
+            ReturnValues: "UPDATED_NEW"
+        }
+
+        console.log(params);
+
+        docClient.update(params, function (err, data) {
+            if (err) {
+                console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+            }
+        });
+    };
+
 
     const [patients, setPatients] = useState(null);
 
@@ -130,7 +201,7 @@ export default function ProfilePatient() {
         symptom10: '',
         symptom11: '',
         comments: '',
-        doctorId: ''
+        flag: ''
     });
 
     const [editFormData, setEditFormData] = useState({
@@ -159,7 +230,7 @@ export default function ProfilePatient() {
         symptom10: '',
         symptom11: '',
         comments: '',
-        doctorId: ''
+        flag: ''
     });
 
     const [editPatientId, setEditPatientId] = useState(null);
@@ -203,7 +274,7 @@ export default function ProfilePatient() {
             symptom10: patient.symptom10,
             symptom11: patient.symptom11,
             comments: patient.comments,
-            doctorId: patient.doctorId
+            flag: patient.flag
         };
 
         setEditFormData(formValues);
@@ -251,7 +322,7 @@ export default function ProfilePatient() {
             symptom10: editFormData.symptom10,
             symptom11: editFormData.symptom11,
             comments: editFormData.comments,
-            doctorId: editFormData.doctorId
+            flag: editFormData.flag
         };
         const newPatients = [...patients];
         const index = patients.findIndex((patient) => patient.id === editPatientId);
@@ -259,6 +330,7 @@ export default function ProfilePatient() {
         setPatients(newPatients);
         // setEditPatientId(null);
         //TODO: add database update function call here
+        updateData('patients', newPatients[0]);
         console.log(JSON.stringify(newPatients));
         console.log("index=", index);
     };
@@ -296,7 +368,7 @@ export default function ProfilePatient() {
             symptom10: editFormData.symptom10,
             symptom11: editFormData.symptom11,
             comments: editFormData.comments,
-            doctorId: editFormData.doctorId
+            flag: editFormData.flag
         };
         setNotifyDoctor(editedNotifyDoctor);
         console.log(JSON.stringify(editedNotifyDoctor));
