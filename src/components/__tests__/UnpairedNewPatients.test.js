f1db0d7d0098b88688cd600e5b41b141cbbea0c6
Jasmin <jasmin.guay@mail.concordia.ca>
2022-03-25 11:36:15 -0400

    Added act function in some tests

--- src/components/__tests__/UnpairedNewPatients.test.js
+++ src/components/__tests__/UnpairedNewPatients.test.js
@@ -1,14 +1,13 @@
-import { render, screen } from "@testing-library/react";
-import DoctorTabs from '../Dashboard/DoctorDashboard/DoctorTabs';
+import { render, screen , act} from "@testing-library/react";
 import UnpairedPatientDoctorEmergencyListTable from '../Dashboard/AdminDashboard/UnpairedPatientDoctorEmergency'
 
 
 test("if the patient list tab is rendered when the user is a doctor", async () => {
-    render(<UnpairedPatientDoctorEmergencyListTable />);
+    act( () => render(<UnpairedPatientDoctorEmergencyListTable />));
     //expect(screen.getByText("Doctor Emergency pairing")).toBeInTheDocument();
 });
 
 test("if the statistics tab is rendered when the user is a doctor", async () => {
-    render(<UnpairedPatientDoctorEmergencyListTable />);
+    act( () => render(<UnpairedPatientDoctorEmergencyListTable />));
     //expect(screen.getByText("Statistics")).toBeInTheDocument();
 });
--- src/components/__tests__/UserProfileFlagTest.test.js
+++ src/components/__tests__/UserProfileFlagTest.test.js
@@ -1,6 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import UserProfileFacade from "../UserProfile/UserProfileFacade";
+import {act} from "react-dom/test-utils";
 
 test("flag button rendering for doctor on patient profile page", async () => {
-    render(<UserProfileFacade/>); //TODO : develop a new test.
+    act( () => render(<UserProfileFacade/>)); //TODO : develop a new test.
 });
