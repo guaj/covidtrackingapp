4b7462d0f4c5d53df7cc5ac0c5cdab07be8f2a92
maya <maya.mcrae@icloud.com>
2022-04-06 03:07:14 -0400

    added tests

--- cypress/integration/covidtracing_spec.js
+++ cypress/integration/covidtracing_spec.js
@@ -1,32 +1,17 @@
 // //test 1
-// describe('health official can send a notification to a patient to fill contact tracing form', () => {
-//     it('health official clicks on send button', () => {
-//         cy.visit("http://localhost:3000/") //open browser to localhost: 3000
-//         cy.findByRole('textbox', { name: /this.example@email.com/i }).type('ho3@gmail.com')// select email field and enter email : ho3@gmail.com
-//         cy.findByTestId('sign-up-password').type('Healthyasfuck1!')// select password field and enter password :  Healthyasfuck1!
-//         cy.findByText(/login/i).click()// click on login
-//         cy.findByRole('tab', { name: /contact tracing list/i }).click()//go to contact tracing tab on dashboard
-//         cy.wait(2000) //for the db to update the send notification column
-//         cy.get('.button').contains('SEND').click()//click send button to send a notification and changes the button to sent once done
-//     })
-// })
-
-//test 2
-describe("patients can fill out the contract tracing form", () =>{
-    it('patient clicks on contact tracing button on profile', () => {
+describe('health official can send a notification to a patient to fill contact tracing form', () => {
+    it('health official clicks on send button', () => {
         cy.visit("http://localhost:3000/") //open browser to localhost: 3000
-        cy.findByRole('textbox', {name: /this.example@email.com/i}).type('eyal@gmail.com')// select email field and enter email : eyal@gmail.com
-        cy.findByTestId('sign-up-password').type('P1234567!')// select password field and enter password :  P1234567!
+        cy.findByRole('textbox', { name: /this.example@email.com/i }).type('ho3@gmail.com')// select email field and enter email : ho3@gmail.com
+        cy.findByTestId('sign-up-password').type('Healthyasfuck1!')// select password field and enter password :  Healthyasfuck1!
         cy.findByText(/login/i).click()// click on login
-        cy.visit("http://localhost:3000/tracing-form") //open browser to localhost: 3000
-        cy.get('#locationName').type('Walmart')//type location name in textbox
-        cy.get('#locationNumber').type('514-665-6060')//type location number in textbox
-        cy.get('#locationDate').type('April 1, 2022')//type location date in textbox
-        cy.get('#locationTime').type('15:00')//type location time in textbox
-        cy.findByRole('button', {name: /add/i})// select add button
-        cy.findByRole('button', {name: /submit/i})// select submit button
+        cy.findByRole('tab', { name: /contact tracing list/i }).click()//go to contact tracing tab on dashboard
+        cy.wait(2000) //for the db to update the send notification column
+        cy.get('.button').contains('SEND').click()//click send button to send a notification and changes the button to sent once done
     })
 })
+
+//test 2
 describe("patients can fill out the contract tracing form", () =>{
     it('patient clicks on contact tracing button on profile', () => {
         cy.visit("http://localhost:3000/") //open browser to localhost: 3000
@@ -45,7 +30,6 @@ describe("patients can fill out the contract tracing form", () =>{
     })
 })
 
-etByRole('button', { name: /account of current user/i })
 //test 3
 describe('health official can access locations list of patients who completed tracing form', () => {
     it('health official clicks on locations list tab', () => {
