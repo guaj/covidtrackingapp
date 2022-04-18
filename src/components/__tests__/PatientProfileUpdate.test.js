69e50ffaaafb52e5f13910d7aef0a7b05901d311
mnash <m.nashatelsayed@gmail.com>
2022-04-18 16:20:19 -0400

    - added admin dashboard user management unit tests

--- src/components/__tests__/PatientProfileUpdate.test.js
+++ src/components/__tests__/PatientProfileUpdate.test.js
@@ -1,12 +1,12 @@
-   import {render, screen} from "@testing-library/react"
-   import PatientProfileUpdate from "../../Services/ProfileUpdateSercices/PatientProfileUpdate/PatientProfileUpdate"
+import {render, screen} from "@testing-library/react"
+import PatientProfileUpdate from "../../Services/ProfileUpdateSercices/PatientProfileUpdate/PatientProfileUpdate"
 
-   test("Check that the name fields in the form is rendered", () => {
-       render(<PatientProfileUpdate/>);
-       expect(screen.getByText("Name")).toBeInTheDocument();
-   })
+test("Check that the name fields in the form is rendered", () => {
+    render(<PatientProfileUpdate/>);
+    expect(screen.getByText("Name")).toBeInTheDocument();
+})
 
-   test("Check that the address fields in the form is rendered", () => {
-       render(<PatientProfileUpdate/>);
-       expect(screen.getByText("Address")).toBeInTheDocument();
-   })
+test("Check that the address fields in the form is rendered", () => {
+    render(<PatientProfileUpdate/>);
+    expect(screen.getByText("Address")).toBeInTheDocument();
+})
